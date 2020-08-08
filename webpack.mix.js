7e0312e192d43a1f60072c33d0af27b5d8eb88af
Thiago Rodrigues <thiagothogs@gmail.com>
2020-08-08 16:36:39 -0300

    add grid system for content

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix
+    .js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+
+    .scripts(
+        'node_modules/popper.js/dist/umd/popper.js',
+        'node_modules/bootstrap/dist/js/bootstrap.js',
+    );
