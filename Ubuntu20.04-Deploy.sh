39a01bbfbc6549965e7c308365a3bdbe3e141349
Thiago Rodrigues <thiagothogs@gmail.com>
2020-08-09 05:19:27 -0300

    update Ubuntu20.04-Deploy.sh

--- Ubuntu20.04-Deploy.sh
+++ Ubuntu20.04-Deploy.sh
@@ -5,6 +5,9 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y nginx php7.4-common php7.4-cli php7.4-gd php7.4-mysql php7.4-curl php7.4-intl php7.4-mbstring php7.4-bcmath php7.4-imap php7.4-xml php7.4-zip php7.4-fpm unzip mysql-server
 
+# Move Project
+cd .. && sudo mv erp-crm-laravel/ /var/www/html/
+
 # Install Composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'e5325b19b381bfd88ce90a5ddb7823406b2a38cff6bb704b0acc289a09c8128d4a8ce2bbafcd1fcbdc38666422fe2806') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
@@ -52,8 +55,7 @@ sudo systemctl start nginx
 sudo systemctl start php7.4-fpm
 
 # Prepare
-cd .. && sudo chown -R www-data:www-data erp-crm-laravel/
-sudo mv erp-crm-laravel/ /var/www/html/
+sudo chown -R www-data:www-data /var/www/html/erp-crm-laravel/
 sudo systemctl restart php7.4-fpm
 sudo systemctl restart nginx
 
