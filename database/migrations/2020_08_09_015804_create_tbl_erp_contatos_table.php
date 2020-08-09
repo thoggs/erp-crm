442d2006423330e48feb373183ef66173faa56f1
Thiago Rodrigues <thiagothogs@gmail.com>
2020-08-08 23:08:02 -0300

    add database configuration

---
+++ database/migrations/2020_08_09_015804_create_tbl_erp_contatos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblErpContatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_erp_contatos', function (Blueprint $table) {
+            $table->id()->autoIncrement()->primary();
+            $table->string('name');
+            $table->string('estado');
+            $table->string('cidade');
+            $table->string('mail');
+            $table->date('data')->useCurrent();
+            $table->string('status')->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_erp_contatos');
+    }
+}
